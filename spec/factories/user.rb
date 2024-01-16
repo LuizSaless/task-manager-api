1d2edbb89a77cd69668abb2bdce91127f5181c7e
LuizSaless <luiznew7@hotmail.com>
2024-01-16 13:50:16 -0300

    Adding initial tests to user model

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
     factory :user do
+        # name { "Luiz" }
         email { "luiznew7@hotmail.com" }
         password { "123456" }
         password_confirmation { "123456" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,35 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 #   pending "add some examples to (or delete) #{__FILE__}"
-before { @user = FactoryBot.build(:user) }
+# let(:user) { build(:user) }
 
-it { expect(@user).to respond_to(:email) }
-# it { expect(@user).to respond_to(:name) }
-# it { expect(@user).to respond_to(:password) }
-# it { expect(@user).to respond_to(:password_confirmation) }
-# it { expect(@user).to be_valid }
+    # it { is_expected.to validate_presence_of(:email) }
+    # it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    # it { is_expected.to validate_confirmation_of(:password) }
+    # it { is_expected.to allow_value("luiznew7@hotmail.com").for(:email) } 
+  
+      # it { expect(user).to respond_to(:email) }
+
+      # it { expect(user).to validate_presence_of(:name) }
+
+
+    # context 'when name is blank' do
+    #   before { user.name = " " } 
+
+    #   it { expect(user).not_to be_valid }
+    # end
+
+  #  context 'when name is nil' do
+  #    before { user.name = nil } 
+
+  #    it { expect(user).not_to be_valid }
+  #  end
+
+  before { @user = FactoryBot.build(:user) }
+
+  it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:name) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
+  it { expect(@user).to be_valid }
 end
